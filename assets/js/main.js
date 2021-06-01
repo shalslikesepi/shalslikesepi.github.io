c84ca80818b7ff5df44e6f9e261daa09b9da486e
Shalini Nair <72762507+shalslikesepi@users.noreply.github.com>
2021-05-31 21:20:39 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -134,32 +134,27 @@
       }
     }
   });
-
+  
   // Porfolio isotope and filter
   $(window).on('load', function() {
     var portfolioIsotope = $('.portfolio-container').isotope({
       itemSelector: '.portfolio-item',
       layoutMode: 'fitRows'
     });
-
     $('#portfolio-flters li').on('click', function() {
       $("#portfolio-flters li").removeClass('filter-active');
       $(this).addClass('filter-active');
-
       portfolioIsotope.isotope({
         filter: $(this).data('filter')
       });
     });
-
   });
-
   // Initiate venobox (lightbox feature used in portofilo)
   $(document).ready(function() {
     $('.venobox').venobox({
       'share': false
     });
   });
-
   // Portfolio details carousel
   $(".portfolio-details-carousel").owlCarousel({
     autoplay: true,
@@ -167,10 +162,4 @@
     loop: true,
     items: 1
   });
-
 })(jQuery);
-
-
-
-
-
